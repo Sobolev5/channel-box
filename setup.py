3c4c940f0eb668dd662506c7578c7d01792bb807
Andrey Sobolev <email.asobolev@gmail.com>
2021-10-26 19:43:52 +0300

    Sobolev:FixReadme

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 import setuptools
 
 __author__ = 'Sobolev Andrey <email.asobolev@gmail.com>'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
